76a8e7f00ef8dd0c3af74c46e183262f6757f230
Ralf Grubenmann <ralf.grubenmann@sdsc.ethz.ch>
2026-04-17 12:26:06 +0200

    add default policies on server start

--- components/backend/internal/middleware/rbac.go
+++ components/backend/internal/middleware/rbac.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/casbin/casbin/v3"
 	entadapter "github.com/casbin/ent-adapter"
@@ -9,11 +10,31 @@ import (
 	"github.com/swissdatasciencecenter/hackagon/components/backend/internal/config"
 )
 
+type Role int
+
+const (
+	Admin Role = iota
+	Owner
+	Member
+)
+
+var roleName = map[Role]string{
+	Admin:  "admin",
+	Owner:  "owner",
+	Member: "member",
+}
+
+func (r Role) String() string {
+	return roleName[r]
+}
+
 type ObjectType int
 
 const (
 	Hackathon ObjectType = iota
 	Team
+	// Note: this is a dummy entry, there are no rules for users, since we only use admin checks with users.
+	// This is just here so we have something we can query on when checking admin permissions for the user table.
 	User
 )
 
@@ -57,7 +78,7 @@ func NewRBACEnforcer(cfg *config.Config) (*Enforcer, error) {
 		return nil, err
 	}
 
-	if _, err = e.AddRoleForUser(cfg.Server.AdminEmail, "admin"); err != nil {
+	if err = defaultPolicies(cfg, e); err != nil {
 		return nil, err
 	}
 
@@ -67,6 +88,23 @@ func NewRBACEnforcer(cfg *config.Config) (*Enforcer, error) {
 	}
 
 	return &Enforcer{enforcer: e}, nil
+}
+
+func defaultPolicies(cfg *config.Config, e *casbin.Enforcer) error {
+	policies := [][]string{
+		{Owner.String(), "*", Hackathon.String(), Read.String()},  // Owner can read owned hackathon
+		{Owner.String(), "*", Hackathon.String(), Write.String()}, // Owner can write owned hackathon
+		{Member.String(), "*", Hackathon.String(), Read.String()}, // Member can read joined hackathon
+	}
+	if _, err := e.AddPolicies(policies); err != nil {
+		return fmt.Errorf("couldn't load grouping policies: %w", err)
+	}
+
+	if _, err := e.AddNamedGroupingPolicy("g2", []string{cfg.Server.AdminEmail, "admin"}); err != nil {
+		return fmt.Errorf("couldn't add default admin: %w", err)
+	}
+
+	return nil
 }
 
 func (e *Enforcer) Enforce(ctx context.Context, hackathonId string, object ObjectType, permission Permission) (bool, error) {
