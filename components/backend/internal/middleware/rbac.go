94170ac89cdeef88326985053215ef5552c839b2
Ralf Grubenmann <ralf.grubenmann@sdsc.ethz.ch>
2026-04-17 12:07:52 +0200

    add rbac to user list endpoint

--- components/backend/internal/middleware/rbac.go
+++ components/backend/internal/middleware/rbac.go
@@ -1,13 +1,53 @@
 package middleware
 
 import (
+	"context"
+
 	"github.com/casbin/casbin/v3"
 	entadapter "github.com/casbin/ent-adapter"
 	_ "github.com/lib/pq"
 	"github.com/swissdatasciencecenter/hackagon/components/backend/internal/config"
 )
 
-func NewRBACEnforcer(cfg *config.Config) (*casbin.Enforcer, error) {
+type ObjectType int
+
+const (
+	Hackathon ObjectType = iota
+	Team
+	User
+)
+
+var objectName = map[ObjectType]string{
+	Hackathon: "hackathon",
+	Team:      "team",
+	User:      "user",
+}
+
+func (ot ObjectType) String() string {
+	return objectName[ot]
+}
+
+type Permission int
+
+const (
+	Read Permission = iota
+	Write
+)
+
+var permissionName = map[Permission]string{
+	Read:  "read",
+	Write: "write",
+}
+
+func (p Permission) String() string {
+	return permissionName[p]
+}
+
+type Enforcer struct {
+	enforcer *casbin.Enforcer
+}
+
+func NewRBACEnforcer(cfg *config.Config) (*Enforcer, error) {
 	a, _ := entadapter.NewAdapter("postgres", cfg.ConnectionStr())
 	e, _ := casbin.NewEnforcer("data/test/config/casbin_model.conf", a)
 
@@ -26,5 +66,14 @@ func NewRBACEnforcer(cfg *config.Config) (*casbin.Enforcer, error) {
 		return nil, err
 	}
 
-	return e, nil
+	return &Enforcer{enforcer: e}, nil
+}
+
+func (e *Enforcer) Enforce(ctx context.Context, hackathonId string, object ObjectType, permission Permission) (bool, error) {
+	sub, err := GetSubject(ctx)
+	if err != nil {
+		return false, err
+	}
+
+	return e.enforcer.Enforce(sub, hackathonId, object.String(), permission.String())
 }
