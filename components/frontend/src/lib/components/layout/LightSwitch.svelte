5b014dbdc2cdfaf5cc50d81a030e722b3595ea43
Hannah Casey <61227037+hanaCasey@users.noreply.github.com>
2026-04-24 17:28:44 +0200

    fix: icon error

--- components/frontend/src/lib/components/layout/LightSwitch.svelte
+++ components/frontend/src/lib/components/layout/LightSwitch.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-    import { Sun, Moon } from 'lucide-svelte';
+    import Sun from 'lucide-svelte/icons/sun';
+    import Moon from 'lucide-svelte/icons/moon';
 
     let checked = $state(false);
 
