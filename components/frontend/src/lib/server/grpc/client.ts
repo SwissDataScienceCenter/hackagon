cc864ef9c910855fd8c137ba462a9d896fcdfd72
Sabine Maennel <5292683+sabinem@users.noreply.github.com>
2026-07-28 11:58:26 +0200

    feat: add page api client to the frontend

--- components/frontend/src/lib/server/grpc/client.ts
+++ components/frontend/src/lib/server/grpc/client.ts
@@ -2,9 +2,11 @@ import { createChannel, createClientFactory, Metadata } from "nice-grpc"
 import { HealthServiceDefinition } from "./generated/health/health_service"
 import { UserServiceDefinition } from "./generated/user/user_service"
 import { HackathonServiceDefinition } from "./generated/hackathon/hackathon_service"
+import { PageServiceDefinition } from "./generated/hackathon/page_service"
 import type { HealthServiceClient } from "./generated/health/health_service"
 import type { UserServiceClient } from "./generated/user/user_service"
 import type { HackathonServiceClient } from "./generated/hackathon/hackathon_service"
+import type { PageServiceClient } from "./generated/hackathon/page_service"
 
 const channel = createChannel("localhost:3000")
 
@@ -25,6 +27,7 @@ export interface AuthorizedGrpc {
   user: UserServiceClient
   health: HealthServiceClient
   hackathon: HackathonServiceClient
+  page: PageServiceClient
 }
 
 export function createAuthorizedGrpc(accessToken: string): AuthorizedGrpc {
@@ -42,6 +45,7 @@ export function createAuthorizedGrpc(accessToken: string): AuthorizedGrpc {
     user: factory.create(UserServiceDefinition, channel),
     health: factory.create(HealthServiceDefinition, channel),
     hackathon: factory.create(HackathonServiceDefinition, channel),
+    page: factory.create(PageServiceDefinition, channel),
   }
 }
 
